4bf162adc0e8a83ad6db51ae204b90ebe3f30534
jeniffer soares <jeniffersoares849@gmail.com>
2026-06-25 11:00:03 -0300

    Adding music to the Level1 Adding FPS and Timeout

--- Code/Level.py
+++ Code/Level.py
@@ -1,4 +1,10 @@
+import sys
+
 import pygame.display
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from Code.Const import COLOR_WHITE, WIN_HEIGHT
 from Code.Entity import Entity
 from Code.EntityFactory import EntityFactory
 
@@ -11,12 +17,31 @@ class Level:
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('summer'))
+        self.timeout = 20000 #20 Seconds
 
     def run(self):
+        pygame.mixer_music.load(f'./Asset/placeful-song-loop-.wav') #Level1 Music
+        pygame.mixer_music.play(-1)
+        clock = pygame.time.Clock()
         while True:
+            clock.tick(40)
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest = ent.rect)
                 ent.move()
-            pygame.display.flip()
-            pass
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: #Close window in the level1
+                    pygame.quit()
+                    sys.exit()
 
+            # Printed text
+            self.level_text(14,f'{self.name} - Timeout: {self.timeout / 1000 :.1f}s', COLOR_WHITE, (10, 5))
+            self.level_text(14, f'fps: {clock.get_fps() :.0f}', COLOR_WHITE, (10, WIN_HEIGHT - 35))
+            self.level_text(14, f'entitades: {len(self.entity_list)}', COLOR_WHITE, (10, WIN_HEIGHT - 20))
+            pygame.display.flip()
+        pass
+
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name = "Lucida Sans Typewriter" , size = text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(left = text_pos[0], top = text_pos[1])
+        self.window.blit(source = text_surf, dest = text_rect)
--- Code/Menu.py
+++ Code/Menu.py
@@ -10,7 +10,7 @@ from Code.Const import MENU_OPTION, COLOR_WHITE, COLOR_YELLOW, COLOR_GREEN, WIN_
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./Asset/demo.jpg')
+        self.surf = pygame.image.load('./Asset/demo.jpg').convert_alpha()
         self.rect = self.surf.get_rect(left = 0, top= 0)
 
     def run(self, ):
